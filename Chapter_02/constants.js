e52c474425ae164b854b4ee7ae636b928d6dfc00
Mike Jackson <perigran_falcon@yahoo.com>
2024-01-11 16:16:38 -0700

    Delete Chapter_02 directory

--- Chapter_02/constants.js
+++
@@ -1,9 +0,0 @@
-// constants.js
-
-const myName = "Maaike";
-const myAge = 29;
-const coder = true;
-const message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
-
-console.log(message);
-
--- Chapter_02/string_variables.js
+++
@@ -1,9 +0,0 @@
-// string_variables.js
-
-let singleString = 'Hi there!';
-let doubleString = "How are you?";
-
-let language = "JavaScript";
-let message = `Let's learn ${language}`;
-console.log(message);
-
