a7c59475eb37a813f3f7fcc0e141cdf4e85be251
Mike Jackson <perigran_falcon@yahoo.com>
2024-01-11 13:57:11 -0700

    Add files via upload

---
+++ Chapter_02/miles_to_kilometer.js
@@ -0,0 +1,7 @@
+// miles_to_kilometers.js
+
+let myDistanceMiles = 130;      //1 mile equals 1.60934 kilometers.
+let myDistanceKM = myDistanceMiles * 1.60934;
+
+console.log("The distance of " + myDistanceMiles + " miles is equal to " + myDistanceKM + " kilometers");
+
---
+++ Chapter_02/number_variables.js
@@ -0,0 +1,11 @@
+// number_variables.js
+
+let intNr = 1;
+let decNr = 1.5;
+let expNr = 1.4e15;
+
+let bigNr = 90071992547409920n;
+
+// typeError
+let result = bigNr + intNr;
+
